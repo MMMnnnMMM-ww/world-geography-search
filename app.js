2058a91c215d796ca00322dacd9243bdea3b2414
MMMnnnMMM-ww <kkongdankkongseo@gmail.com>
2026-08-30 17:08:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 document.querySelector("button").onclick = function() {
     console.log("Search clicked");
+    let searchText = document.querySelector("input").value;
+    console.log(searchText);
 };
